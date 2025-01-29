bc4b8a96736af1cb05a6c91766082ca12e0a08c6
Md Samsuddoha <mdsamsuddoha@Mds-MacBook-Pro-2.local>
2025-01-29 16:14:35 +0600

    L21

--- Lecture 21/Practice_21.sql
+++ Lecture 21/Practice_21.sql
@@ -9,7 +9,7 @@
 --Q2: Find the name, designation and basic salary of each employee who are from IT department and receive salary more than 50,000.
 
 
---Q3: Find the number of employees in each department.
+--Q3: Count the number of employees in each department.
 
 
 
@@ -17,3 +17,23 @@
 
 
 --Q5: Show the name and salary of each employee with an increment of 10% on Net salary.
+
+
+--Q6: Find the list of employee who took more than 3 leave.
+
+
+--Q7: Display all employees who joined after January 1, 2020.
+
+
+--Q8: Fetch employees with the designation "Manager".
+
+
+--Q9: Calculate the total salary paid to all employees.
+
+
+--Q10: Find the highest and lowest net salary among all employees.
+
+--Q11: Display the average salary for each department.
+
+--Q12: Find the department with the maximum number of employees.
+
